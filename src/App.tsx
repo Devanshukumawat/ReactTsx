758336401efcab20400af91a512cecc08378fe5c
Devanshukumawat <devanshukumawat063@gmail.com>
2024-03-20 15:42:28 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import DrawerAppBar from './components/Appbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+   <DrawerAppBar/>
+    <Routes>
+      <Route path='/login' element={<Login/>}/> 
+      <Route path='/signup' element={<Signup/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+    
+    
   );
 }
 
